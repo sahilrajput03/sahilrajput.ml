97abb6be96b272e74114b30a8a489b4444981975
Sahil Rajput <sahilrajput03@gmail.com>
2022-09-20 00:04:01 +0530

    up

---
+++ components/Comments.js
@@ -0,0 +1,20 @@
+import Giscus from '@giscus/react'
+
+export default function Comments() {
+	return (
+		<Giscus
+			id='comments'
+			repo='sahilrajput03/sahilrajput.ml'
+			repoId='MDEwOlJlcG9zaXRvcnkzNTExODQwMjQ='
+			category='Announcements'
+			categoryId='DIC_kwDOFO6kmM4CRhM5'
+			mapping='pathname'
+			term='Welcome to @giscus/react component!'
+			reactionsEnabled='1'
+			emitMetadata='0'
+			inputPosition='top'
+			theme='preferred_color_scheme'
+			lang='en'
+		/>
+	)
+}
--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,21 @@
 import 'nextra-theme-blog/style.css'
 import Head from 'next/head'
 import '../styles/line-highlight.css' // Yo!!
+import Giscus from '@giscus/react'
 
 import '../styles/main.css'
 import '../styles/who-am-i.scss'
+import {useEffect, useState} from 'react'
+import {useRouter} from 'next/router'
+import ClientOnly from 'components/ClientOnly'
 
 export default function Nextra({Component, pageProps}) {
+	// const router = useRouter()
+
+	// useEffect(() => {
+	// 	alert(router.asPath) // prints the `location.pathname` yikes!
+	// }, [])
+
 	return (
 		<>
 			<Head>
